6b60bce573b890d57d452518d4d7b864f178d728
Roshani-Poojari <roshanipoojari2002@gmail.com>
2024-07-12 20:36:26 +0530

    first commit

---
+++ Model/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Model
+{
+    internal class Product
+    {
+        private int _prodId;
+        private string _prodName;
+        private double _prodPrice;
+        private double _prodDiscountPercent;
+        private const double discountMax = 40;
+        private const double discountMin = 20;
+        public int Id { set { _prodId = value; } get { return _prodId; } }
+        public string Name { set { _prodName = value; } get { return _prodName; } }
+        public double Price { set { _prodPrice = value; } get {return _prodPrice;} }
+        public double DiscountPercent { 
+            set {  _prodDiscountPercent = value;
+                if (value > discountMax)
+                    _prodDiscountPercent = 40;
+                else if (value < discountMin)
+                    _prodDiscountPercent = 20;
+                else
+                    _prodDiscountPercent = value;
+            } 
+            get { return _prodDiscountPercent;} 
+        }
+        public Product()//default constructor
+        {
+            Console.WriteLine("Default constructor");
+        }
+        public Product(int id, string name, double price, double discountPercent):this()//parameterized constructor
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            DiscountPercent = discountPercent;
+        }
+
+        public double PriceAfterDiscount() {
+            return _prodPrice - (_prodPrice * _prodDiscountPercent) / 100;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.Model;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(1,"Television",50000,10);
+            Console.WriteLine($"Product id: {product1.Id} \n" +
+                $"Product name: {product1.Name} \n" +
+                $"Actual price: {product1.Price} \n" +
+                $"Discount %: {product1.DiscountPercent} \n" +
+                $"Price after discount: {product1.PriceAfterDiscount()}");
+
+
+            Console.WriteLine("========================================================================");
+
+            Product product2 = new Product(2,"Smart phone",19000,50);
+            Console.WriteLine($"Product id: {product2.Id} \n" +
+                $"Product name: {product2.Name} \n" +
+                $"Actual price: {product2.Price} \n" +
+                $"Discount %: {product2.DiscountPercent} \n" +
+                $"Price after discount: {product2.PriceAfterDiscount()}");
+
+        }
+    }
+}
